2b0350ab62e9d38764924da8b0b65ed0f492d683
g013r <hectorsegundo23@gmail.com>
2023-08-19 16:34:19 -0500

    proyecto casi final

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const deskMenu = document.querySelector('.desktop-menu')
 const menuLogo = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const menuCarrito = document.querySelector('.navbar-shopping-cart')
-const productDetail = document.querySelector('.product-detail')
+const productDetail = document.querySelector('#shopping')
+const toggleDetail = document.getElementById("product-detail")
 const cardsContainer = document.querySelector('.cards-container')
+const detailClose = document.querySelector('.product-detail-close')
 
 menuEmail.addEventListener('click', ()=>{
   if (!productDetail.classList.contains('inactive')) {
@@ -13,9 +15,13 @@ menuEmail.addEventListener('click', ()=>{
   deskMenu.classList.toggle('inactive');
 })
 
+
 menuLogo.addEventListener('click', ()=>{
   if (!productDetail.classList.contains('inactive')) {
     productDetail.classList.add('inactive')
+  }
+  if (!toggleDetail.classList.contains('inactive')) {
+    toggleDetail.classList.add('inactive');
   }
   mobileMenu.classList.toggle('inactive');
 })
@@ -28,7 +34,17 @@ menuCarrito.addEventListener('click', ()=>{
   if (!deskMenu.classList.contains('inactive')) {
     deskMenu.classList.add('inactive')
   }
+  if (!toggleDetail.classList.contains('inactive')) {
+    toggleDetail.classList.add('inactive')
+  }
   productDetail.classList.toggle('inactive')
+})
+
+detailClose.addEventListener('click',()=>{
+  if (!productDetail.classList.contains('inactive')){
+    productDetail.classList.add('inactive')
+  }
+  toggleDetail.classList.add('inactive')
 })
 
 //compras
@@ -41,25 +57,50 @@ productList.push({
 
 })
 productList.push({
-  name: 'Bike',
-  price: 120,
+  name: 'television',
+  price: 300,
   Image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 
 })
 
-for(products of productList){
-  cardsContainer.innerHTML += `
-  <div class="product-card">
-    <img src= ${products.Image} alt="">
-    <div class="product-info">
-      <div>
-      <p>${products.price}</p>
-      <p>${products.name}</p>
-      </div>
-      <figure>
-      <img src="./icons/bt_add_to_cart.svg" alt="">
-      </figure>
-    </div>
-  </div>
-  `
+function arrayInsert(arr) {
+  for(products of arr){
+    const productCard = document.createElement('div')
+    productCard.classList.add("product-card")
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', products.Image)
+    productImg.addEventListener('click', ()=>{
+      toggleDetail.classList.remove('inactive');
+    })
+
+    const productInfo = document.createElement('div')
+    productInfo.classList.add("product-info")
+
+    const productInfoDiv = document.createElement('div')
+
+    const productprice = document.createElement('p')
+    productprice.innerText = products.price;
+
+    const productName = document.createElement('p')
+    productName.innerText = products.name;
+
+    productInfoDiv.appendChild(productprice)
+    productInfoDiv.appendChild(productName)
+
+    const productInfoFigure = document.createElement('figure')
+    const productImgCart = document.createElement('img')
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+    productInfoFigure.appendChild(productImgCart)
+
+    productInfo.appendChild(productInfoDiv)
+    productInfo.appendChild(productInfoFigure)
+
+    productCard.appendChild(productImg)
+    productCard.appendChild(productInfo)
+
+    cardsContainer.appendChild(productCard)
+  }
 }
+
+arrayInsert(productList)
